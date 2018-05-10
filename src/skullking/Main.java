244015e022f76b55856f5e313818fcff1b52a024
moon <moon@DESKTOP-BVGL0UP>
2018-05-11 01:27:40 +0900

    the very first version

---
+++ src/skullking/Main.java
@@ -0,0 +1,11 @@
+package skullking;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The first version");
+
+	}
+
+}
---
+++ src/skullking/Player.java
@@ -0,0 +1,5 @@
+package skullking;
+
+public interface Player {
+
+}
