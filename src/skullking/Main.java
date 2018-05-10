105c46fbc844a08a19a07580d1b28e1427343e02
moon <moon@DESKTOP-BVGL0UP>
2018-05-11 01:46:25 +0900

    second

--- src/skullking/Main.java
+++ src/skullking/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("The first version");
+		System.out.println("Second");
 
 	}
 
