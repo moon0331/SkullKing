39e5efa2f63b788c5116bf9bc5c055432eed0c36
moon <moon@DESKTOP-BVGL0UP>
2018-05-14 21:07:09 +0900

    grade(등급), number(숫자) 필드 // hasBonusScore 메서드는 스컬킹의 경우 보너스점수

--- src/skullking/Card.java
+++ src/skullking/Card.java
@@ -1,5 +1,22 @@
 package skullking;
 
 public class Card implements CardInfo {
-
+	public int grade; 
+	public int number; //일반카드 경우 숫자
+	/*등급 
+	 * (skullking, 
+	 * pirate, 
+	 * changeable, 
+	 * mermaid, 
+	 * black, 
+	 * gold, red, blue, 
+	 * escape)
+	 * 
+	 * enum으로 처리할지 생각중
+	*/
+	
+	public boolean hasBonusScore() {
+		//return grade==SKULLKING;
+		return grade==100;
+	}
 }
