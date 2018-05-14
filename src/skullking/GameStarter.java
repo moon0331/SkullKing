6f76dd450a313d1295cf223efa4df27952d61f10
moon <moon@DESKTOP-BVGL0UP>
2018-05-14 16:54:47 +0900

    플레이어 수 결정, 플레이어 등록 함수

--- src/skullking/GameStarter.java
+++ src/skullking/GameStarter.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class GameStarter {
 	
-	int numOfPlayer;
+	private int numOfPlayer;
+	protected Player[] p;
+	private String[] NameList= {"Momo", "Joy", "Kei", "Solar", "Jisoo"};
 	
 	public int setNumOfPlayer() {
 		Scanner s=new Scanner(System.in);
@@ -24,12 +26,11 @@ public class GameStarter {
 		return numOfPlayer;
 	}
 	
-	public Player[] makePlayer() {
-		Player[] p=new Player[numOfPlayer];
+	public void makePlayer() {
+		p=new Player[numOfPlayer];
 		for(int i=0; i<p.length; i++) {
-			
+			p[i].name=NameList[i]; //이름 결정(나중에 입력할지?), 나머지 값들은 0으로 초기화됨
 		}
-		return p;
 	}
 
 }
