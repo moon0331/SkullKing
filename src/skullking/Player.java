b4269e4a92c7f6bb3b3dc38b8b466b868e3531b0
moon <moon@DESKTOP-BVGL0UP>
2018-05-14 21:12:35 +0900

    player 클래스.

--- src/skullking/Player.java
+++ src/skullking/Player.java
@@ -1,7 +1,12 @@
 package skullking;
 
-public class Player implements PlayerInfo {
-	int predict_win;
-	int score;
+import java.util.List;
+import java.util.ArrayList;
 
+public class Player implements PlayerInfo {
+	String name;
+	int score;
+	int predict_num_of_win;
+	boolean won_previous_game;
+	private List<Card> deck;
 }
