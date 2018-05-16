8f8a94c9838e1069345d89409da47d2c37015f88
moon <moon@DESKTOP-BVGL0UP>
2018-05-16 15:11:18 +0900

    main 수정중

--- src/skullking/Main.java
+++ src/skullking/Main.java
@@ -1,3 +1,5 @@
+//https://github.com/moon0331/SkullKing
+
 package skullking;
 
 public class Main {
@@ -5,11 +7,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Game newgame=new Game();
-		int numOfPlayer=newgame.setNumOfPlayer(); //사람 수 결정
-		for(int i=0; i<numOfPlayer; i++) {
-			newgame.makePlayer(i); //플레이어 생성
-		}
+		
+		GameStarter newgame=new GameStarter();
+		newgame.setNumOfPlayer(); //사람 수 결정
+		
+		int numOfPlayer=newgame.getNumOfPlayer();
+		
+		newgame.makePlayer(); //플레이어 생성 (나중에 이름 받을거면 parameter 수정가능)
+		
 		System.out.println("Game Start!");
 		for(int i=0;i<10;i++) {
 			int winner=newgame.playGame(i);
