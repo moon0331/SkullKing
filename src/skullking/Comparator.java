1334854860c98818880b4c81b006b7a9a392d229
moon <moon@DESKTOP-BVGL0UP>
2018-05-16 15:39:21 +0900

    comparator

--- src/skullking/Comparator.java
+++ src/skullking/Comparator.java
@@ -1,6 +1,33 @@
 package skullking;
 
-public class Comparator implements ComparatorInfo {
+public class Comparator implements ComparatorInfo { //구조체 필요?
 	//카드를 내는 곳으로 정의함
-
+	Card present_card;
+	int present_card_owner;
+	Card strongest_card;
+	int strongest_card_owner;
+	int constraint;
+	// int winner;
+	
+	public void setCard(int owner, Card card) {
+		present_card=card;
+		present_card_owner=owner;
+		//if() constraint= 제약조건 걸리는 때
+	}
+	public void compare() {
+		if(strongest_card==null) {
+			strongest_card=present_card;
+			strongest_card_owner=present_card_owner;
+		}else {
+			if(stronger()) { //실제 세기 비교하는
+				//교체
+			}
+		}
+	}
+	public int getWinner() {
+		return strongest_card_owner;
+	}
+	public boolean stronger() { //카드 세기 비교
+		return false;
+	}
 }
--- src/skullking/ComparatorInfo.java
+++ src/skullking/ComparatorInfo.java
@@ -1,5 +1,8 @@
 package skullking;
 
 public interface ComparatorInfo {
-	
+	public void setCard(int owner, Card card); //카드 놓기
+	public void compare(); //현재 카드와 놓은 카드 비교, 승자 결정
+	public int getWinner();
+	public boolean stronger();
 }
