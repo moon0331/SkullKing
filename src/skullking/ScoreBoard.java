d4c6a5875183acb060b411b4d12188c639534c04
NahahaNahaha <hanwg000@naver.com>
2018-05-20 15:44:46 +0900

    board
    다른 클래스 다 파고 걍 짜집기해서 올릴겡

--- src/skullking/ScoreBoard.java
+++ src/skullking/ScoreBoard.java
@@ -1,5 +1,12 @@
 package skullking;
 
-public class ScoreBoard implements ScoreBoardInfo {
+public class ScoreBoard implements ScoreBoardInfo  {
+	
+	public int score; 
+	score=Player.score; 
+	
+
+	System.out.println("Player의 점수는 "+score);
+    
 
 }
