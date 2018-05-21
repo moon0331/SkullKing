27d304a3654ce58802e8d5492104779ae5ea22c1
moon0331 <moon0331@skku.edu>
2018-05-22 00:37:32 +0900

    Update Card.java

--- src/skullking/Card.java
+++ src/skullking/Card.java
@@ -1,10 +1,10 @@
 package skullking;
 
 public class Card implements CardInfo {
-	
-	private int Card_num; 
-	private String Card_type;//�Ϲ�ī�� ��� ����
-	/*��� 
+	private int idx;
+	private String card_type;//카드 타입 string꼴로 표현하든지 아님 int로 표현하든지
+	private int card_num; 
+	/*
 	 * (skullking, 
 	 * pirate, 
 	 * scarymarry-해적으로도 쓸 수 있고 항복기로도 쓸 수 있는, 
@@ -13,42 +13,54 @@ public class Card implements CardInfo {
 	 * gold, red, blue, 
 	 * escape)
 	 * 
-	 * enum���� ó������ ������
+	 * enum 나중에 사용할수도 있음
 	*/
-    public Card(String Card_type, int Card_num) //카드 생성자 Card_type은 스컬킹, 해적,인어, 골드 ,레드 등등 종류를 받을거고 Card_num은 카드번호 예를 들면 블루 12 이렇게 1~13의 값을 가질 수 있도록 
+	
+	//getter setter 다시 수정함.
+	
+	/* 반복문으로 연속적으로 생성자를 불러오기 때문에, 생성자는 하나의 모양으로 통일해야 함. public Card(int i)로 idx=i로 만든 후, 
+	 * i의 범위에 따라 카드의 특징을 분류(if절 활용)해야 함. 
+	 * (Ex: 0번은 escape, 1~13은 red, 14~26은 blue, ..., n번은 skullking으로 한다면,
+	 * 각각의 범위에 따라 card_num가 card_type가 달라질 것임)
+	 * idx가 primary key의 역할을 한다 보면 됨. (필요없어지면 지울거임)
+	 */
+	
+	public Card(int i) {
+		idx=i;
+		//i의 범위에 따라 카드 정보 설정
+	}
+	
+    /*public Card(String Card_type, int Card_num) //카드 생성자 Card_type은 스컬킹, 해적,인어, 골드 ,레드 등등 종류를 받을거고 Card_num은 카드번호 예를 들면 블루 12 이렇게 1~13의 값을 가질 수 있도록 
     {
-    	this.Card_type=Card_type; 
-    	this.Card_num=Card_num;
+    	this.card_type=card_type; 
+    	this.card_num=card_num;
     } 
     
     public Card(String Card_type)//스컬킹이나 해적같은 경우는 숫자카드들이랑 다르게 숫자가 없으므로 생성자를 두 개 만들어서 직관성을 높임 
     {
     	this.Card_type=Card_type; 
         this.Card_num=0; 
-    } 
+    } */
     
-   public int getCardNum()
-   {
-	   return Card_num;
-   }
-   
-   public void setCardNum()
-   {
-	   this.Card_num=Card_num;
-   }
-   
-   public String getCardType()
-   {
-	   return Card_type;
-   } 
-   
-   public String setCardType()
-   {
-	   this.Card_type=Card_type;
-   }
-   //getter 와 setter 들 메드에 '_' 들어가면 안된다해서 저렇게 씀 원래는 getCard_type으로 쓰고 싶었음 ㅋㅋ; 
-   
-   if(Card_type='skullking')
+	public String getType() {
+		return card_type;
+	}
+	
+	public void setType(String type) {
+		card_type=type;
+	}
+	
+	public int getNum() {
+		return card_num;
+	}
+	
+	public void setNum(int num) {
+		card_num=num;
+	}
+	
+	//보너스 점수
+	
+   /*if(Card_type='skullking')
    {
 	   Card_num=16; //생각해봤는데 카드넘버로 세기 비교가능해 숫자카드들은 1~13이고 인어는 14 해적은 15 스컬킹은 16 이렇게 하면 딱 세기 비교 가능하니까 
    } 
@@ -79,7 +91,7 @@ public class Card implements CardInfo {
    if(Card_type='escape')
    {
 	   Card_num=0;
-   }
+   }*/
    
    
    
