833615e5b6d12e35c22e4734ee13a66f594c976e
moon <moon@DESKTOP-BVGL0UP>
2018-05-22 21:07:36 +0900

    main

--- src/skullking/Main.java
+++ src/skullking/Main.java
@@ -3,27 +3,24 @@
 package skullking;
 
 public class Main {
-	final int NUMBER_OF_CARD=65; //맞나?
+	final int NUMBER_OF_CARD=65;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		GameStarter newgame=new GameStarter();
-		newgame.setNumOfPlayer(); //사람 수 결정
-		
-		int numOfPlayer=newgame.getNumOfPlayer(); //사람수
-		
-		newgame.makePlayer(); //플레이어 생성 (나중에 이름 받을거면 parameter 수정가능)
+		newgame.setNumOfPlayer(); //determine the number of player
+		Player[] player=newgame.makePlayer(); //make player data
 		
 		System.out.println("Game Start!");
 		for(int round=1;round<=10;round++) {
-			Game game=new Game(newgame);
-			int winner=game.playGame(round); //round장 뽑아서 게임 진행
-			// 우승자 가리고
-			// 점수계산
+			Game game=new Game(player);
+			game.playGame(round); //real game process
+			int winner=game.getWinner();
+			// calculate scores
 		}
-		//우승자 가리기
-		//종료
+		int player_winner=-1;//represent winner (name can be changed)
+		System.out.println("Winner is : "+player_winner);
 	}
 
 }
