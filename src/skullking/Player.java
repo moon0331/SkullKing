95090c5062be12f5ff3cc1a5ab61a2120d3d7489
moon <moon@DESKTOP-BVGL0UP>
2018-05-22 21:02:31 +0900

    player

--- src/skullking/Player.java
+++ src/skullking/Player.java
@@ -9,4 +9,17 @@ public class Player implements PlayerInfo {
 	int predict_num_of_win;
 	boolean won_previous_game;
 	private List<Card> deck;
+	
+	public void addCard(Card c) { //add card from game-deck
+		deck.add(c);
+	}
+	public Card playCard(int i) { //play a card
+		return deck.get(i);
+	}
+	public void setPredictWin(int numOfWin) { //predict the number of win
+		predict_num_of_win=numOfWin;
+	}
+	public void calScore(int score) { //plus(or minus) score
+		this.score+=score;
+	}
 }
--- src/skullking/PlayerInfo.java
+++ src/skullking/PlayerInfo.java
@@ -1,5 +1,6 @@
 package skullking;
 
 public interface PlayerInfo {
-
+	public void addCard(Card c);
+	public Card playCard(int i);
 }
