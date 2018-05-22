a4c4fb923b5186f7cbe3123bcd996af0caddb6d7
moon <moon@DESKTOP-BVGL0UP>
2018-05-22 21:29:11 +0900

    읽어보고 이해 안되는 부분 있으면 얘기해주셈

--- src/skullking/Game.java
+++ src/skullking/Game.java
@@ -14,24 +14,27 @@ public class Game{
 		comparator=new Comparator();
 	}
 	
-	public int playGame(int round) { //game process
+	public void playGame(int round) { //game process
 		Scanner s=new Scanner(System.in);
-		for(int i=0; i<p.length; i++) {
-			for(int j=0; i<round; i++) {
+		for(int i=0; i<p.length; i++) { //for all player
+			for(int j=0; i<round; j++) {
 				p[i].addCard(deck.pickCard()); //each player pick card in deck round times
 			}
 			
 			System.out.println("player "+ i +" turn to predict win: ");
 			int win=s.nextInt(); // exception needed!!!!!!!!!!!!!!!!!!!!!
 			p[i].setPredictWin(win); //prediction
-		}
+		} 
+		//now, each player has their own card in p.cardDeck, and all players predicted their number to win.
 		
 		for(int i=0; i<p.length; i++) {
-			System.out.println("put the card!");
-			
+			System.out.println("put the card! : Player"+i);
+			Card c=p[i].playCard(i); //each player play a card 
+			comparator.setCard(i,c); //on comparator
 		}
+		winner=comparator.getWinner();
+		
 		s.close();
-		return getWinner();
 	}
 	public void setWinner(int p) {
 		winner=p;
