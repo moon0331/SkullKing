33958ced6aefbe528850e6e3ce93b4531994ff11
moon <moon@DESKTOP-BVGL0UP>
2018-05-22 21:18:01 +0900

    player 수정

--- src/skullking/Player.java
+++ src/skullking/Player.java
@@ -2,6 +2,7 @@ package skullking;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Player implements PlayerInfo {
 	String name;
@@ -13,8 +14,16 @@ public class Player implements PlayerInfo {
 	public void addCard(Card c) { //add card from game-deck
 		deck.add(c);
 	}
-	public Card playCard(int i) { //play a card
-		return deck.get(i);
+	public Card playCard(int constraint) { //play a card
+		Scanner s=new Scanner(System.in);
+		int val=-1;
+		while(true) {
+			val=s.nextInt(); //exception needed!!!!!!!!!!!
+			if(true) /*in this case, you should modify the condition that only pick the right card*/
+				break; 
+		}
+		s.close();
+		return deck.get(val);
 	}
 	public void setPredictWin(int numOfWin) { //predict the number of win
 		predict_num_of_win=numOfWin;
