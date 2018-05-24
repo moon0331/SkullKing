a0433c61df8f5929b93210a2b0e78d14283201a1
moon <moon@DESKTOP-BVGL0UP>
2018-05-24 23:53:52 +0900

    버퍼 문제로 인한 무한루프 돔 (playCard에서)

--- src/skullking/Player.java
+++ src/skullking/Player.java
@@ -11,6 +11,10 @@ public class Player implements PlayerInfo {
 	private boolean won_previous_game;
 	private List<Card> deck;
 	
+	public Player() {
+		deck=new ArrayList<Card>();
+	}
+	
 	public void setName(String s) {
 		name=s;
 	}
@@ -20,22 +24,41 @@ public class Player implements PlayerInfo {
 	}
 	
 	public void addCard(Card c) { //add card from game-deck
+		System.out.println("player "+ name +" adds Card.");
 		deck.add(c);
 	}
 	public Card playCard(int constraint) { //play a card
-		Scanner s=new Scanner(System.in);
-		int val=-1;
-		while(true) {
-			val=s.nextInt(); //exception needed!!!!!!!!!!!
-			if(true) /*in this case, you should modify the condition that only pick the right card*/
-				break; 
+		System.out.println("input number to pick");
+		int val;
+		while(true){
+			Scanner s=new Scanner(System.in);
+			try {
+				val=s.nextInt();
+				if(val>=0 && val<deck.size()) break;
+			} catch(Exception e){
+				System.out.println("Exception : unvalid input");
+			}finally {
+				s.close();
+			}
 		}
-		s.close();
 		deck.remove(val);
 		return deck.get(val);
 	}
-	public void setPredictWin(int numOfWin) { //predict the number of win
-		predict_num_of_win=numOfWin;
+	public void setPredictWin(int round) { //predict the number of win
+		System.out.println(name +"'s turn to predict win: ");
+		Scanner s=new Scanner(System.in);
+		try {
+			predict_num_of_win=s.nextInt(); // why NoSuchElementException ?????
+			if(predict_num_of_win<0 || predict_num_of_win>round) {
+				System.out.println("Not Valid number of win. Set prediction to zero.");
+				predict_num_of_win=0;
+			}
+		} catch(Exception e){
+			System.out.println("win number: "+predict_num_of_win);
+			System.out.println("======Exception! Set the number of win zero.");
+			predict_num_of_win=0;
+		}
+		s.close();
 	}
 	public void calScore(int score) { //plus(or minus) score
 		this.score+=score;
