ff2cfd054f33be57682c12978ef93fd542f4ea15
moon <moon@DESKTOP-BVGL0UP>
2018-05-28 23:12:58 +0900

    playCard인자 잠시 수정함

--- src/skullking/PlayerInfo.java
+++ src/skullking/PlayerInfo.java
@@ -6,7 +6,7 @@ public interface PlayerInfo {
 	public void setName(String s);
 	
 	public void addCard(Card c);
-	public Card playCard(int constraint);
+	public Card playCard(int round, int constraint);
 	public void setPredictWin(int numOfWin);
 	public void calScore(int score);
 }
