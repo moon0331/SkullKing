68af30d4318fbef2293a1d742866393a1a2f6ca3
moon <moon@DESKTOP-BVGL0UP>
2018-05-28 23:13:07 +0900

    game

--- src/skullking/Game.java
+++ src/skullking/Game.java
@@ -28,10 +28,11 @@ public class Game{
 		
 		for(int i=0; i<p.length; i++) {
 			System.out.println("put the card! : Player"+i);
-			Card c=p[i].playCard(standard_card); //each player play a card 
-		    if(standard_card==-1&&c>=5&&c<=56)//기준이 되는 카드가 없고 내려놓은 카드가 숫자카드일 경우,escape 카드는 0부터 4까지
+			Card c=p[i].playCard(round, standard_card); //each player play a card 
+			int cardNum=c.getNum();
+		    if(standard_card==-1 && cardNum>=5 && cardNum<=56)//기준이 되는 카드가 없고 내려놓은 카드가 숫자카드일 경우,escape 카드는 0부터 4까지
 		    {
-		       standard_card=c;
+		       standard_card=cardNum;
 		    }
 			
 			comparator.setCard(i,c); //on comparator
